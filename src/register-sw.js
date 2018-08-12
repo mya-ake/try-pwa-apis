5eb89b12ac3495e77229eacca2c63ed1abec7318
mya-ake <make.app.ts@gmail.com>
2018-08-12 18:37:50 +0900

    feat:  save

---
+++ src/register-sw.js
@@ -0,0 +1,16 @@
+import { usableServiceWorker } from './lib/check-api';
+import logger from './lib/logger';
+
+export default () => {
+  if (usableServiceWorker() === false) {
+    return;
+  }
+  window.navigator.serviceWorker
+    .register('./sw.js')
+    .then(registration => {
+      logger.name('sw').info('success', registration);
+    })
+    .catch(err => {
+      logger.name('sw').error(err);
+    });
+};
