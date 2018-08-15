2f195d92cd8b3cf81bf78529eee2b669170d7cef
mya-ake <make.app.ts@gmail.com>
2018-08-15 14:44:02 +0900

    feat: mjs もトランスパイルの対象に追加

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,8 @@ module.exports = {
   lintOnSave: true,
   productionSourceMap: false,
   configureWebpack: externalWebpackConfig,
+  chainWebpack: config => {
+    // .mjs もトランスパイルの対象とするため
+    config.module.rule('js').test(/\.m?jsx?$/);
+  },
 };
