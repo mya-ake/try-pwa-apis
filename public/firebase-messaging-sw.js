d36936e7a561324c1b2a27fb7e4ee3321dcde7c7
mya-ake <make.app.ts@gmail.com>
2018-08-20 01:58:44 +0900

    feat: Update messaging sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,3 +6,21 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(payload => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload,
+  );
+  const { data } = payload;
+
+  const notificationTitle = data.title;
+  const notificationOptions = {
+    body: data.body,
+    icon: '/icons/android-chrome-192x192.png',
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions,
+  );
+});
