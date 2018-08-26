fcd97c8da7939cfea6666d320a4248ed189914d6
mya-ake <make.app.ts@gmail.com>
2018-08-26 17:47:59 +0900

    feat: Update BaseButton

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,8 +1,15 @@
 <template>
   <button 
     ref="button" 
-    class="base-button mdc-button mdc-button--raised"
-    @click="$emit('click')"
+    :class="{
+      'mdc-button--raised': raised,
+      'base-button--raised': raised,
+      'mdc-button--unelevated': unelevated,
+      'base-button--unelevated': unelevated,
+      'base-button--processing': processing,
+    }"
+    class="base-button mdc-button"
+    @click="handleClick"
   >
     <slot/>
   </button>
@@ -14,6 +21,29 @@ import { MDCRipple } from '@material/ripple';
 export default {
   name: 'BaseButton',
 
+  props: {
+    raised: {
+      type: Boolean,
+      default: false,
+    },
+
+    unelevated: {
+      type: Boolean,
+      default: false,
+    },
+
+    asyncOnClick: {
+      type: Function,
+      default: null,
+    },
+  },
+
+  data() {
+    return {
+      processing: false,
+    };
+  },
+
   mounted() {
     this.initializeMDC();
   },
@@ -21,6 +51,20 @@ export default {
   methods: {
     initializeMDC() {
       new MDCRipple(this.$refs.button);
+    },
+
+    async handleClick() {
+      this.$emit('click');
+      if (typeof this.asyncOnClick !== 'function') {
+        return;
+      }
+      if (this.processing === true) {
+        console.log('skip');
+        return;
+      }
+      this.processing = true;
+      await this.asyncOnClick();
+      this.processing = false;
     },
   },
 };
@@ -30,8 +74,19 @@ export default {
 @import '@material/button/mdc-button';
 
 .base-button {
+  @include mdc-button-ink-color(teal);
+  @include mdc-states(teal);
+}
+
+.base-button--unelevated,
+.base-button--raised {
   @include mdc-button-container-fill-color(teal);
   @include mdc-button-ink-color(white);
   @include mdc-states(white);
+}
+
+.base-button--processing {
+  opacity: 0.6;
+  cursor: not-allowed;
 }
 </style>
