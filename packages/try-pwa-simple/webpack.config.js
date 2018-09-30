094a3d3eea9e8ec76f8cb679bd1d9bcd07f75fc4
mya-ake <make.app.ts@gmail.com>
2018-09-30 16:03:18 +0900

    feat: Update

--- packages/try-pwa-simple/webpack.config.js
+++ packages/try-pwa-simple/webpack.config.js
@@ -3,7 +3,10 @@ const { GenerateSW } = require("workbox-webpack-plugin");
 module.exports = {
   plugins: [
     new GenerateSW({
-      swDest: "sw.js"
+      swDest: "sw.js",
+      skipWaiting: true,
+      clientsClaim: true,
+      navigateFallback: "/index.html"
     })
   ]
 };
