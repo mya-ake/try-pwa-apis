78422681a1d03921e8c373e32160978036ec6c1a
mya-ake <make.app.ts@gmail.com>
2018-10-01 01:33:26 +0900

    refactor: Unnecessary code

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 const install = (Vue, config) => {
-  if (name === null) {
-    throw new Error(`Require name @storage plugin`);
-  }
-
   const client = axios.create(config);
   Vue.prototype.$_axios = client;
 };
